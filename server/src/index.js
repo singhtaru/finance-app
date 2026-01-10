281c226723bd58048052a4e4a139d2065ef1cc71
Taru <taru93858@gmail.com>
2026-01-10 16:51:13 +0530

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -32,7 +32,7 @@ app.use(cors({
   },
   credentials: true
 }));
-app.options("*", cors());
+app.options("/*", cors());
 app.use(express.json());
 app.use(passport.initialize()); // Initialize passport after app creation
 app.use("/api/auth", authRoutes);
